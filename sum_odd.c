8e4ae84fc4989e5522471cfd5be40ec355507132
Samir Raja <samirttt888@gmail.com>
2026-01-12 16:20:28 +0530

    sum_odd.c

--- sum_odd.c
+++ sum_odd.c
@@ -1,14 +1,22 @@
-#include <stdio.h>
-int main(){
-    int num;
-    printf("enter a number");
-    scanf("%d", &num);
-    if(num %2==0){
-        printf("%d is even",num);
-    }
-    else{
-        printf("%d is odd",num);
+/*
+ * Module: Parity Checker (Even/Odd Logic)
+ * Author: Samir Raja
+ * Description: Determines integer parity using modulus operations.
+ */
 
+#include <stdio.h>
+
+int main() {
+    int num;
+    
+    printf("\n[>] Enter an Integer to analyze: ");
+    scanf("%d", &num);
+
+    if(num % 2 == 0) {
+        printf("[SUCCESS] %d is an EVEN number.\n", num);
+    } else {
+        printf("[SUCCESS] %d is an ODD number.\n", num);
     }
+    
     return 0;
 }
